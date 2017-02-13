703afc1101b7062c0f0104fe1901f081f95d3c1f
Romanini <lahaisty@gmail.com>
2017-02-12 22:18:11 -0400

    iniciando o projeto

---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Rondônia Top</title>
+	<link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+	<header>
+		<div class="topbar">
+			<div id="topbar-left" class="login">
+				
+			</div>
+
+			<div id="topbar-rigth">
+				<div id="sociais">
+					<ul>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+						<li><a href=""></a></li>
+					</ul>
+				</div>
+
+				<div id="menu-rigth">
+					
+				</div>
+			</div>
+		</div>
+
+		<div class="logo">
+			<h1>
+				<a href=""><img src="" alt=""></a>
+			</h1>
+		</div>
+
+		<div class="anuncio-topo">
+			
+		</div>
+	</header>
+
+	<div class="search">
+		<div class="pesquisa">
+			
+		</div>
+
+		<div class="fuso">
+			
+		</div>
+	</div>
+</body>
+</html>
